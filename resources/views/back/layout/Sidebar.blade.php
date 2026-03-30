eb98a980da8cd9ebb84df04c61f7de628caff326
gadeerdawwas <up120161676@gmail.com>
2026-03-30 13:30:04 +0300

    logo

--- resources/views/back/layout/Sidebar.blade.php
+++ resources/views/back/layout/Sidebar.blade.php
@@ -7,9 +7,13 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 d-flex">
             {{--  <div class="image">  --}}
-                <img src="{{ asset('front/black.png') }}" class="img-circle elevation-2" alt="User Image">
+                <a href="{{ route('dashboard') }}">
+                {{--  <img src="{{ asset('front/black.png') }}" class="img-circle elevation-2" alt="User Image">  --}}
+               <img src="{{ asset('front/logo1.png') }}" class="img-circle elevation-2" alt="User Image">
+
+                </a>
             {{--  </div>  --}}
-           
+
         </div>
 
 
